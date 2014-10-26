b66761db45ab3e583de50cdcbc82dd972700b972
Adrien Ghosn <ghosn.adrien@gmail.com>
2014-10-26 22:07:10 +0100

    Adding the plugin
    Just the skeleton for now.

---
+++ plugin/src/main/scala/Plugin.scala
@@ -0,0 +1,19 @@
+/**
+ * Main component of the compiler plugin.
+ *
+ *@author Adrien Ghosn
+**/
+
+import scala.tools.nsc.{ Global, Phase, SubComponent }
+import scala.tools.nsc.plugins.{ Plugin => NscPlugin, PluginComponent => NscPluginComponent }
+
+class Plugin(val global: Global) extends NscPlugin {
+	import global._
+
+	val name = "Obey"
+	val description = """Compile plugin that checks defined rules against scalameta trees.
+	http://github.com/aghosn/Obey for more informations."""
+	val components: List[NscPluginComponent] = Nil
+
+
+}
