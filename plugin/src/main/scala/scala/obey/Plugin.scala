4a9893a1b901013fd6679a8a7dcb715f3c83e38f
Adrien Ghosn <ghosn.adrien@gmail.com>
2014-10-28 21:17:46 +0100

    Adding annotations
    Not sure about how it is goind to be integrated inside the rules ...

--- plugin/src/main/scala/scala/obey/Plugin.scala
+++ plugin/src/main/scala/scala/obey/Plugin.scala
@@ -16,7 +16,7 @@ class Plugin(val global: Global) extends NscPlugin {
 	http://github.com/aghosn/Obey for more informations."""
 	val components: List[NscPluginComponent] = List(ObeyPluginComponent)
 
-	
+
 	private object ObeyPluginComponent extends NscPluginComponent {
 		import global._
 
@@ -28,9 +28,8 @@ class Plugin(val global: Global) extends NscPlugin {
 		/**TODO will be responsible for calling the loader*/
 		def newPhase(prev: Phase): Phase = new StdPhase(prev) {
 			def apply(unit: CompilationUnit) {
-				/** TODO implement the meet here*/
+				/** TODO implement the meat here*/
 			}
-		}
-		
+		}	
 	}
 }
