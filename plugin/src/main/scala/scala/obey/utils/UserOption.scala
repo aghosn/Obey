eb840ec1cdb3ad017fcf52639bd360979585badc
aghosn@abel <ghosn.adrien@gmail.com>
2014-11-17 00:12:29 +0100

    Modified the user options
    Now we have a more flexible interface for the user that respects the default behavior I defined.
    Only warnings are accepted by default. All rules are applied.
    The user needs to explicitly activate the format and abort, and explicitly deactivate the report

--- plugin/src/main/scala/scala/obey/utils/UserOption.scala
+++ plugin/src/main/scala/scala/obey/utils/UserOption.scala
@@ -6,16 +6,29 @@ import scala.obey.model.Rule
 
 object UserOption {
 
-  case class holder(var pos: Set[Tag], var neg: Set[Tag])
+  case class holder(var pos: Set[Tag], var neg: Set[Tag], var use: Boolean)
 
-  val all: holder = holder(Set(), Set())
-  val format: holder = holder(Set(), Set())
-  val report: holder = holder(Set(), Set())
-  val abort: holder = holder(Set(), Set())
+  val all: holder = holder(Set(), Set(), true)
+  val format: holder = holder(Set(), Set(), false)
+  val report: holder = holder(Set(), Set(), true)
+  val abort: holder = holder(Set(), Set(), false)
 
   def filterFormat: Set[Rule] = filterT(all.pos ++ format.pos, all.neg ++ format.neg)
 
   def filterReport: Set[Rule] = filterT(all.pos ++ report.pos, all.neg ++ report.neg)
 
   def filterAbort: Set[Rule] = filterT(all.pos ++ abort.pos, all.neg ++ abort.neg)
+
+  /* Get rules for a holder according to the 'all' filter*/
+  def getRules(h: holder): Set[Rule] = {
+  	if(!h.use)
+  		return Set()
+  	val allRules: Set[Rule] = filterT(all.pos, all.neg)
+  	filter(h.pos, h.neg)(allRules)
+  }
+
+  /* All methods to get the correct rules to apply*/
+  def getFormat: Set[Rule] = getRules(format)
+  def getReport: Set[Rule] = getRules(report)
+  def getAbort: Set[Rule] = getRules(abort)
 }
