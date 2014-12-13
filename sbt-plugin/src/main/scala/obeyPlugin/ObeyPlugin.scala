12f577ff18557d35fbad34af55854b27fa048895
Adrien Ghosn <ghosn.adrien@gmail.com>
2014-12-13 09:47:34 +0100

    Trying to purge the plugin

---
+++ sbt-plugin/src/main/scala/obeyPlugin/ObeyPlugin.scala
@@ -0,0 +1,27 @@
+package obeyplugin
+import sbt._
+import Keys._
+
+
+object ObeyPlugin extends AutoPlugin {
+  val obeyFix = settingKey[Seq[String]]("List of tags to filter rewritting rules.")
+  val obeyWarn = settingKey[Seq[String]]("List of tags to filter warning rules.")
+  val obeyRules = settingKey[String]("Path to .class defined by the user.")
+
+  lazy val helloCommand = 
+    Command.command("hello") { (state: State) => 
+      println("HIIIII")
+      state
+    }
+
+  override lazy val projectSettings: Seq[sbt.Def.Setting[_]] = Seq(
+    obeyFix := Seq.empty,
+    obeyWarn := Seq.empty,
+    obeyRules := "",
+
+    addCompilerPlugin("com.github.aghosn" % "plugin_2.11.2" % "0.1.0-SNAPSHOT"),
+    scalacOptions ++= Seq(
+      "-P:obey:format:" + obeyFix,
+      "-P:obey:warn:" + obeyWarn,
+      "-P:obey:addRules:" + obeyRules.value).filterNot(x => x.endsWith(":")))
+}
