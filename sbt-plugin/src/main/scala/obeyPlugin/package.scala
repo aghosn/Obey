e465c6fc689be0258c3b72dc023fef1329528f1d
Adrien Ghosn <ghosn.adrien@gmail.com>
2014-12-13 16:34:43 +0100

    URRRRRRRRRRRRRRRRRRRF

--- sbt-plugin/src/main/scala/obeyPlugin/package.scala
+++ sbt-plugin/src/main/scala/obeyPlugin/package.scala
@@ -6,11 +6,17 @@ package object obeyplugin {
   val obeyWarn = settingKey[String]("List of tags to filter warning rules.")
   val obeyRules = settingKey[String]("Path to .class defined by the user.")
 
+  lazy val helloCommand =
+    Command.command("hello") { (state: State) =>
+      println("Hi!")
+      state
+    }
+
   lazy val obeySettings: Seq[sbt.Def.Setting[_]] = Seq(
     obeyFix := "",
     obeyWarn := "",
     obeyRules := "",
-  
+    commands += helloCommand,
     addCompilerPlugin("com.github.aghosn" % "plugin_2.11.2" % "0.1.0-SNAPSHOT"),
     scalacOptions ++= Seq(
       "-P:obey:format:" + obeyFix.value,
