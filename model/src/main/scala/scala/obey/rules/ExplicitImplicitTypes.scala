e5b8532416a8bb7802d124d7054f2a4eb45a13ad
Adrien Ghosn <ghosn.adrien@gmail.com>
2015-01-15 21:02:10 +0100

    Implemented/corrected the ExplicitImplict
    Need to test it

--- model/src/main/scala/scala/obey/rules/ExplicitImplicitTypes.scala
+++ model/src/main/scala/scala/obey/rules/ExplicitImplicitTypes.scala
@@ -5,37 +5,29 @@ import scala.meta.tql.ScalaMetaFusionTraverser._
 import scala.meta.internal.ast._
 import scala.obey.model._
 import scala.meta.semantic._
+import scala.language.implicitConversions
 
-/*@Tag("Type", "Explicit") class ExplicitImplicitTypes(implicit c: Context) extends Rule {
+@Tag("Type", "Explicit") class ExplicitImplicitTypes(implicit c: Context) extends Rule {
 
 
   def description = "Explicit types in implicit Val & Def"
 
-  def message(t: Defn, tpe: Type.Name) = Message(s"Implicit type $tpe in $t", t)
+  def message(t: Defn, tpe: scala.meta.Type) = Message(s"Implicit type $tpe in $t", t)
 
-  def toExplicit = transform {
-      case tree @ Term.Param(mods, name, None, Some(t)) if mods.exists(_.isInstanceOf[Mod.Implicit]) => 
-        Term.Param(mods, name, Some(t.tpe), Some(t)) andCollect Message(s"Implicit type ${t.tpe} in $tree", tree)
-    }.topDown
-
+  implicit def toType(t: scala.meta.Type) = t.asInstanceOf[scala.meta.internal.ast.Type]
+  implicit def toType2(t: scala.meta.Type): scala.meta.internal.ast.Type.Arg = t.asInstanceOf[scala.meta.internal.ast.Type.Arg]
 
   def apply = {
-    (transform {
+    transform {
       case t @ Defn.Val(mods, pats, None, rhs) if mods.exists(_.isInstanceOf[Mod.Implicit]) =>
         Defn.Val(mods, pats, Some(rhs.tpe), rhs) andCollect message(t, rhs.tpe)
       
       case t @ Defn.Def(mods, name, tparams, paramss, None, body) if mods.exists(_.isInstanceOf[Mod.Implicit]) =>
         Defn.Def(mods, name, tparams, paramss, Some(body.tpe), body) andCollect message(t, body.tpe)
 
-      case Ctor.Primary(mods, paramss) => 
-        val res = toExplicit(paramss)
-        Ctor.Primary(mods, res.tree.get) andCollect res.result
+      case tree @ Term.Param(mods, name, None, Some(t)) if mods.exists(_.isInstanceOf[Mod.Implicit]) =>
+        Term.Param(mods, name, Some(toType2(t.tpe)), Some(t)) andCollect Message(s"Implicit type ${t.tpe} in $tree", tree)
 
-      case Ctor.Secondary(mods, paramss, primaryCtor, stats) => 
-        val res1 = toExplicit(paramss)
-        val res2 = toExplicit(primaryCtor)
-        Ctor.Secondary(mods, res1.tree.get, res2.tree.get, stats) andCollect res1.result ++ res2.result
-
-    }).topDown
+    }.topDown
   }
-} */
+}
